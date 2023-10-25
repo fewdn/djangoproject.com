874c023e0071268d7ed95d7d38c2b4047fa82f89
Owen <username@example.com>
2023-10-24 22:16:22 -0700

    Explored HttpRequest object, polls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    response_msg = "Hello, world. You're at the polls index." \
+                   f"You made a {request.method} request." \
+                   f"The server is running on port {request.META['SERVER_PORT']}."
+    return HttpResponse(response_msg)
